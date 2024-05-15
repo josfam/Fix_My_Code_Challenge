8505b7ff057132300481d939843fda912a5b7257
josfam <jamukun@proton.me>
2024-05-15 18:45:25 +0300

    Formatting: Fix pep

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
-class square():
-    
+
+class square:
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square"""
         return self.width * self.height
 
     def PermiterOfMySquare(self):
@@ -19,6 +19,7 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
